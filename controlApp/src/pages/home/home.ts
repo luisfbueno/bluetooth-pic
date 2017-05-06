9506b66f4a1be11003c454c1424a2056a85e0fa6
Luis <luis.bueno96@gmail.com>
2017-05-06 16:12:29 -0300

    Uso do ngModule para verificar status dos Toggles

--- controlApp/src/pages/home/home.ts
+++ controlApp/src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  public vermelho: boolean;
+  public verde: boolean;
+  public amarelo: boolean;
 
+  
   constructor(public navCtrl: NavController) {
+    this.vermelho = false;
+    this.verde = false;
+    this.amarelo = false;
 
+  } 
+
+  public mudaVermelho() {
+    console.log("Vermelho: "+ this.vermelho); 
   }
+
+  public mudaVerde() {
+    console.log("Verde: "+ this.verde); 
+  }
+
+  public mudaAmarelo() {
+    console.log("Amarelo: "+ this.amarelo); 
+  }
+
 
 }
