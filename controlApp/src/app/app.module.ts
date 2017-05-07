9c375655339c4a9f9a3fe3f36c96cecd63bce24e
Luis <luis.bueno96@gmail.com>
2017-05-07 17:57:29 -0300

    Alerta para conexão do bluetooth

--- controlApp/src/app/app.module.ts
+++ controlApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { FormsModule }   from '@angular/forms';
+import {BluetoothSerial} from '@ionic-native/bluetooth-serial';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothSerial,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- controlApp/src/pages/home/home.ts
+++ controlApp/src/pages/home/home.ts
@@ -1,35 +1,53 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 
-export class HomePage {
+export class HomePage{
   public vermelho: boolean;
   public verde: boolean;
   public amarelo: boolean;
 
-  
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController,public bluetoothSerial: BluetoothSerial) {
     this.vermelho = false;
     this.verde = false;
     this.amarelo = false;
 
-  } 
+    this.connect();
+  }
+
+  public connect(){
+    this.bluetoothSerial.enable().then((status)=> {
+        this.showAlert();
+    });
+  }
+
+  public showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Bluetooth',
+      subTitle: 'Bluetooth enabled',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
   public mudaVermelho() {
-    console.log("Vermelho: "+ this.vermelho); 
+    console.log("Vermelho: "+ this.vermelho);
+    this.showAlert();
   }
 
   public mudaVerde() {
-    console.log("Verde: "+ this.verde); 
+    console.log("Verde: "+ this.verde);
   }
 
   public mudaAmarelo() {
-    console.log("Amarelo: "+ this.amarelo); 
+    console.log("Amarelo: "+ this.amarelo);
   }
 
 
